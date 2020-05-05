8fa0300b7cf5e95c06a1f1dbeafd4490fb036122
magicmirror2020 <56414705+magicmirror2020@users.noreply.github.com>
2020-05-05 18:41:14 -0400

    Update mask.py

--- mask.py
+++ mask.py
@@ -3,10 +3,14 @@ import math
 import webbrowser
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-open_array = []
+zero_array = []
+one_array = []
+two_array = []
 three_array = []
-
-mode = 0
+four_array = []
+five_array = []
+font = cv2.FONT_HERSHEY_SIMPLEX
+line = cv2.LINE_AA
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -59,37 +63,113 @@ while cap.isOpened():
         cv2.line(roi, start, end, [0, 255, 0], 2)
 
     l += 1  # need or else count is down 1
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    if l == 5:
-        open_array.append(l)
-        print(open_array)
-        if len(open_array) == 26:
-            mode = 1
-            if mode == 1:
-                webbrowser.open('http://spotify.com')
-                open_array.clear()  # clears array so it can be used again
-                mode = 0  # needs to be last
-
+    # can make concise later hard code for now
+    # if l == 1:
+    #     if arearatio < 17.5:
+    #         l == 0
+    #         cv2.putText(frame, '0', (0, 50), font, 2, (255, 255, 255), 3, line)
+    #         zero_array.append(l)
+    #         print(zero_array)
+    #         if len(zero_array) == 30:
+    #             #webbrowser.open('http://google.com')
+    #             zero_array.clear()  # clears array so it can be used again
+    #     else:
+    #         cv2.putText(frame, '1', (0, 50), font, 2, (255, 255, 255), 3, line)
+    #         one_array.append(l)
+    #         print(one_array)
+    #         if len(one_array) == 30:
+    #             #webbrowser.open('http://google.com')
+    #             one_array.clear()  # clears array so it can be used again
+    if l == 2:
+        cv2.putText(frame, '2', (0, 50), font, 2, (255, 255, 255), 3, line)
+        two_array.append(l)
+        print(two_array)
+        if len(two_array) == 30:
+            #webbrowser.open(http://192.168.0.16:8080/magicmirror/module/newsfeed/articlemoredetails)
+            two_array.clear()  # clears array so it can be used again
+    elif l == 3:
+        cv2.putText(frame, '3', (0, 50), font, 2, (255, 255, 255), 3, line)
+        three_array.append(l)
+        print(three_array)
+        if len(three_array) == 30:
+            #webbrowser.open('http://google.com')
+            three_array.clear()  # clears array so it can be used again
+    elif l == 4:
+        cv2.putText(frame, '4', (0, 50), font, 2, (255, 255, 255), 3, line)
+        four_array.append(l)
+        print(four_array)
+        if len(four_array) == 30:
+            #webbrowser.open('http://spotify.com')
+            four_array.clear()  # clears array so it can be used again
+    elif l == 5:
+        cv2.putText(frame, '5', (0, 50), font, 2, (255, 255, 255), 3, line)
+        five_array.append(l)
+        print(five_array)
+        if len(five_array) == 30:
+            webbrowser.open('http://google.com')
+            five_array.clear()  # clears array so it can be used again
     else:
-        cv2.putText(frame, 'Nothing', (10, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(frame, 'Waiting', (10, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
     cv2.imshow('Mask', mask)
-    k = cv2.waitKey(2) & 0xFF
-    if k == 27:
+    if cv2.waitKey(2) & 0xFF == 27:
         break
+### Pull
+import json
+import requests
+import time
+requests.adapters.DEFAULT_RETRIES=10
 
-    if l == 1:
-        if areacnt < 5000:  # adjust this for how much mask in frame
-            cv2.putText(frame, 'Waiting for gesture', (0, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
-        else:
-            if arearatio < 17.5:
-                cv2.putText(frame, '0', (0, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
-            else:
-                cv2.putText(frame, '1', (0, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
-    elif l == 2:
-        cv2.putText(frame, '2', (0, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
-    elif l == 3:
-        cv2.putText(frame, '3', (0, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
-    elif l == 4:
-        cv2.putText(frame, '4', (0, 50), font, 2, (255, 255, 255), 3, cv2.LINE_AA)
+# -- newsfeed stuff
+url_newsfeed = "http://192.168.0.16:8080/api/module/newsfeed/?apiKey=magic"
+
+# -- newsfeed get list of commands
+r = requests.get(url_newsfeed, timeout=30)
+newsfeed_commands = json.loads(r.text)
+print(newsfeed_commands)
+
+print('\nNEWSFEED COMMANDS\n')
+print("MODULE: {}".format(newsfeed_commands['module']))
+for action in newsfeed_commands['actions']:
+    print("\tCOMMAND: {}".format(action))
+
+# -- newsfeed articlenext command
+url_newsfeed_articlenext = "http://192.168.0.16:8080/api/module/newsfeed/articlenext/?apiKey=magic"
+r = requests.get(url_newsfeed_articlenext, timeout=30)
+response = json.loads(r.text)
+print(response)
+
+# -- ALERT TEST
+# -- show alert
+url_alert_showalert = "http://192.168.0.16:8080/api/module/alert/showalert?title=title&message=message&apiKey=magic"
+r = requests.get(url_alert_showalert, timeout=30)
+response = json.loads(r.text)
+print(response)
+
+# -- wait 10 seconds
+time.sleep(10)
+
+# -- hide alert
+url_alert_hidealert = "http://192.168.0.16:8080/api/module/alert/hidealert?apiKey=magic"
+r = requests.get(url_alert_hidealert, timeout=30)
+response = json.loads(r.text)
+print(response)
+
+screen_refresh = "http://192.168.0.16:8080/api/refresh"
+screen_brightness = "http://192.168.0.16:8080/api/brightness/100" # 10-200
+toggle_fullscreen = "http://192.168.0.16:8080/api/togglefullscreen" # switches between windowed and fullscreen views
+hide_module = "http://192.168.0.16:8080/api/modules/clock/hide" # replace 'clock' with module names
+show_module = "http://192.168.0.16:8080/api/modules/clock/show"
+hide_all = "http://192.168.0.16:8080/api/modules/all/hide" # mirror mode
+show_all = "http://192.168.0.16:8080/api/modules/all/show" # module mode
+
+# module located here https://github.com/eouia/MMM-Spotify
+spotify_pause = "http://192.168.0.16:8080/api/notification/SPOTIFY_PAUSE"
+spotify_play = "http://192.168.0.16:8080/api/notification/SPOTIFY_PLAY"
+spotify_toggle = "http://192.168.0.16:8080/api/notification/SPOTIFY_TOGGLE" # toggles pause/play
+spotify_next = "http://192.168.0.16:8080/api/notification/SPOTIFY_NEXT"
+spotify_previous = "http://192.168.0.16:8080/api/notification/SPOTIFY_PREVIOUS"
+spotify_shuffle = "http://192.168.0.16:8080/api/notification/SPOTIFY_NEXT"
+spotify_repeat = "http://192.168.0.16:8080/api/notification/SPOTIFY_PREVIOUS"
+# spotify_search = curl -X POST http://192.168.0.16:8080/api/notification/SPOTIFY_SEARCH -H 'content-type: application/json' -d '{"type": "artist,playlist", "query": "michael+jackson", "random": "false"}'
